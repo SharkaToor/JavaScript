32e2665b4dfb418d258109b9b1d5e4ed497a8ecd
Sharka Toor <151713042+SharkaToor@users.noreply.github.com>
2024-02-20 07:40:37 +0000

    basics

---
+++ 01_basics/variabls.js
@@ -0,0 +1,9 @@
+const accountId = 123456
+let accountEmail = "sharka@gmail.com"
+var accountPasword = "12345"
+accountCity = "javascript"
+
+//accountId = 2 //not allowed
+console.log(accountPasword);
+
+console.table([accountEmail, accountId, accountPasword, accountCity])
