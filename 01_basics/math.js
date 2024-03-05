52fff5ebc8b69d6eb06b96ddf7cb3d05457bf53a
Sharka Toor <151713042+SharkaToor@users.noreply.github.com>
2024-03-05 00:28:46 +0000

    nums & maths

---
+++ 01_basics/math.js
@@ -0,0 +1,26 @@
+//const score = 400 
+//console.log(score);
+
+//const balance = new Number (100);
+//console.log(balance);
+
+//console.log (balance.toString().length)
+//console.log (balance.toFixed(1)) //presision values
+
+             //maths
+
+// //abs is absolute value
+//console.log(Math.round(6.4));
+//console.log(Math.round(6.7));
+//console.log(Math.ceil(6.7)); //value will be top values
+//console.log(Math.floor(6.4)); //value will be lower values
+//console.log(Math.min(6, 3, 5, 4)); //value will be lower values
+//console.log(Math.max(6, 3, 5, 4)); //value will be lower values
+
+//console.log(Math.random()) //math random will give uh values between 0 and 1 
+console.log(Math.floor(Math.random()*10) + 1) 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max -min + 1)) + min); // to change the range 
