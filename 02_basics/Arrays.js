6704693ab1d09783265404ba567218ccd8b28591
Sharka Toor <151713042+SharkaToor@users.noreply.github.com>
2024-03-05 02:22:16 +0000

    arrays

---
+++ 02_basics/Arrays.js
@@ -0,0 +1,14 @@
+// arrays
+
+const myArr = [0, 1, 2, 4, 5]
+const myHeros = ["sharka", "imran Khan", "Baba"]
+console.log(myArr[5]); // 0 based indexing in arrays 
+
+console.log(myHeros [0]);
+
+
+//arrays and its methods 
+
+myArr.push(6)
+console.log(myArr);
+
