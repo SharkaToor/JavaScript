1fff5f74a202164755ab64c7f04baa34ca3cef2f
Sharka Toor <151713042+SharkaToor@users.noreply.github.com>
2024-03-05 07:38:55 +0000

    UPDATE

--- 02_basics/Objects.js
+++ 02_basics/Objects.js
@@ -1,1 +1,20 @@
-// 
+// singleton 
+// objects ko literals ki trha declair kren gy to singleton nhi bny ga // constructor sy agr bny ga to fir singleton bny ga 
+
+//object literals
+
+// Object.create  //constructor method jis me singleton bnta hy
+ const JsUser = {
+    name: "sharka",
+    agr : 18,
+    email: "sharka@gmail.com",
+     location: "lahore",
+     isLoggenIn: false,
+     lastLoggedIn: ["monday", "saturday"]
+ }
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+
+//HOW TO TAKE A SYMBOL AND ADD IT INTO AN OBJECTS KEYS 
+
+const mySym = Symbol("key1")
